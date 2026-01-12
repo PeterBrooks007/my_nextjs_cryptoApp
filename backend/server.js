042b10aeb452eb9f552aff3217506cde36c9c079
Peter Brooks <digitalyinvestassets@gmail.com>
2026-01-12 22:15:00 +0100

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://my-nextjs-crypto-app-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://mywebsitesamples.com"],
     methods: ["GET", "POST"],
   },
 });
@@ -49,7 +49,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:3001",
-      "https://my-nextjs-crypto-app-frontend.vercel.app",
+      "https://mywebsitesamples.com",
       "http://localhost:5173",
     ],
     credentials: true,
